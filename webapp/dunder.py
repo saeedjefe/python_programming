f04d7eb9cff27682eef14522ec896bc6c48922f4
saeedjefe <saeedjefe@gmail.com>
2021-06-16 10:37:41 +0430

    wed commit

---
+++ webapp/dunder.py
@@ -0,0 +1,5 @@
+print("we start of with", __name__);
+if __name__ == '__main__':
+    print('and end of with', __name__);
+else:
+    print(__name__);
--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from function_test import hello
 
 
 app = Flask(__name__)
 
-@app.route('/salam')
-def salam() -> str:
-    return "salam";
+def log_request(req: str, res: str) -> None:
+    with open('dirty_grammar.log', 'a') as log:
+        print(req, res, file=log);
 
 @app.route('/dirty_grammar', methods=['POST'])
-def hello( ) -> str:
+def hello( ) -> 'html':
     #get the text and return determiners
     phrase = request.form['phrase'];
     letters = {'a', 'an', 'the', 'your', 'my', 'your', 'his', 'her', 'many', 'much', 'few', 'a few', 'a lot of'};
     split_text = phrase.split(" ");
-    found_determiners = letters.intersection(split_text);
-    return str(found_determiners);
+    results  = str(letters.intersection(split_text));
+    log_request(phrase, results);
+    return render_template('result.html', the_results=results, the_phrase=phrase);
 
+@app.route('/')
 @app.route('/entry')
 def entery_page() -> 'html':
 
