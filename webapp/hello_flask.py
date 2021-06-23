56194fedfb01d3e3b5c6aa5ac6d0b46788526bc6
saeedjefe <saeedjefe@gmail.com>
2021-06-23 15:32:25 +0430

    wed commit

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, escape
 from function_test import hello
 
 
 app = Flask(__name__)
 
-def log_request(req: str, res: str) -> None:
+def log_request(req: 'flask_request', res: str) -> None:
     with open('dirty_grammar.log', 'a') as log:
-        print(req, res, file=log);
+        print( req.form, req.user_agent, req.remote_addr, file=log, sep='|');
+
 
 @app.route('/dirty_grammar', methods=['POST'])
 def hello( ) -> 'html':
@@ -15,7 +16,7 @@ def hello( ) -> 'html':
     letters = {'a', 'an', 'the', 'your', 'my', 'your', 'his', 'her', 'many', 'much', 'few', 'a few', 'a lot of'};
     split_text = phrase.split(" ");
     results  = str(letters.intersection(split_text));
-    log_request(phrase, results);
+    log_request(request, results);
     return render_template('result.html', the_results=results, the_phrase=phrase);
 
 @app.route('/')
@@ -23,5 +24,11 @@ def hello( ) -> 'html':
 def entery_page() -> 'html':
 
     return render_template('entry.html', the_title='welcome to dirty grammar on the web!');
+
+@app.route('/viewlog')
+def show_the_log() -> str:
+    with open('dirty_grammar.log') as view_log:
+        contents = view_log.read();
+    return escape(contents);
 
 app.run(debug=True);
