4203999a434e390d4403c245ebcd922dfb465def
saeedjefe <saeedjefe@gmail.com>
2021-07-21 10:02:52 +0430

    wed

--- webapp/dunder.py
+++
@@ -1,5 +0,0 @@
-print("we start of with", __name__);
-if __name__ == '__main__':
-    print('and end of with', __name__);
-else:
-    print(__name__);
--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, escape
-from function_test import hello
+
 
 
 app = Flask(__name__)
